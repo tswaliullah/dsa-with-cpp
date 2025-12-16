1557c6a651d22deed089f21d0d66d04c4a05de40
MD Waliullah <waliullah.godev@gmail.com>
2025-12-16 10:11:09 +0600

    clear, resize

--- module-2/size_capacity.cpp
+++ module-2/size_capacity.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    
+
     vector<int> v;
 
     // cout << v.capacity() << endl;
@@ -23,9 +23,6 @@ int main()
     // v.push_back(10);
     // cout << v.capacity() << endl;
 
-
-
-
     cout << v.size() << endl;
     v.push_back(10);
     v.push_back(10);
@@ -34,8 +31,18 @@ int main()
     v.push_back(10);
     v.push_back(10);
     v.push_back(10);
+
+    // v.clear();
     cout << v.size() << endl;
-    
-    
+    cout << v[1] << endl;
+    cout << v[2] << endl;
+    cout << v[3] << endl;
+
+    v.resize(11);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
     return 0;
 }
