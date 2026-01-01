4f2e797a4ce13c2a58d2c408d83012445af3dd2c
MD Waliullah <waliullah.godev@gmail.com>
2026-01-01 21:40:15 +0600

    refactor

--- assignment-1/diefferent.cpp
+++ assignment-1/diefferent.cpp
@@ -30,13 +30,15 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
 
 void get_difference(Node *head)
 {
-    int mx = INT_MAX, mn = INT_MIN;
+    int mx = INT_MIN, mn = INT_MAX;
 
     for (Node *i = head; i != NULL; i = i->next)
     {
+        mx = max(mx, i->val);
         mn = min(mn, i->val);
-        mn = max(mn, i->val);
     }
+
+    cout << mx - mn;
 }
 
 int main()
