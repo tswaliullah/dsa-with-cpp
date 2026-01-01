0a18898e2c04bcf31008647cc6dd7a32f5e5ad3b
MD Waliullah <waliullah.godev@gmail.com>
2026-01-01 20:17:55 +0600

    O(N) instead of O(n*n)

---
+++ assignment-1/diefferent.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void get_difference(Node *head)
+{
+    int mx = INT_MAX, mn = INT_MIN;
+
+    for (Node *i = head; i != NULL; i = i->next)
+    {
+        mn = min(mn, i->val);
+        mn = max(mn, i->val);
+    }
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, tail, val);
+    }
+
+    get_difference(head);
+
+    return 0;
+}
