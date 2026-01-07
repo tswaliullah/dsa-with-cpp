7be7574db389cbcd3d47acf2c79b01dee026a761
MD Waliullah <waliullah.godev@gmail.com>
2026-01-07 12:03:19 +0600

    insert into head

---
+++ module-8/insert_at_head.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_into_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = tail;
+    tail->prev = a;
+
+    insert_into_head(head, 100);
+    print_forward(head);
+
+    return 0;
+}
