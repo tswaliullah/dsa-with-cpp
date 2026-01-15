5f1904cd5db9f40de22f4ec36112d072a81b20b2
MD Waliullah <waliullah.godev@gmail.com>
2026-01-15 11:35:41 +0600

    doubly reverse

---
+++ module-10/reverse_doubly_linked_list.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_into_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void reverse_doubly(Node *head, Node *tail)
+{
+    for (Node *i = head, *j = tail; i != j && i->prev != j; i = i->next, j = j->prev)
+    {
+        swap(i->val, j->val);
+    }
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_into_tail(head, tail, val);
+    }
+
+    reverse_doubly(head, tail);
+    print_forward(head);
+
+    return 0;
+}
