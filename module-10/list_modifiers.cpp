84e991b19fdf48dfab6b44e5727b338aeb0014d8
MD Waliullah <waliullah.godev@gmail.com>
2026-01-15 10:08:28 +0600

    delete at any

--- module-10/list_modifiers.cpp
+++ module-10/list_modifiers.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 int main()
 {
-    list<int> l = {1, 2, 3};
+    list<int> l = {1, 2, 3, 4, 5, 6};
     list<int> l2;
     // l2 = l;
-    l2.assign(l.begin(), l.end());
+    // l2.assign(l.begin(), l.end());
 
-    l.push_back(99);
-    l.push_front(100);
+    // l.push_back(99);
+    // l.push_front(100);
 
-    cout << *next(l.begin(), 2); // index
+    // cout << *next(l.begin(), 2); // index
 
-    // for (int val : l)
-    // {
-    //     cout << val << endl;
-    // }
+    // l.insert(next(l.begin(), 1), 33);
+
+    l.erase(next(l.begin(), 2));
+
+    for (int val : l)
+    {
+        cout << val << endl;
+    }
 
     return 0;
 }
