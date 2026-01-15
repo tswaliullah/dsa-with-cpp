d81913883ba2b9a7e8bb29ddc60798f65cd4a6c6
MD Waliullah <waliullah.godev@gmail.com>
2026-01-15 13:24:01 +0600

    cycle detected

---
+++ module-10/detect-cycle.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+
+    head->next = a;
+    a->prev = b;
+    b->next = c;
+    c->next = d;
+    d->next = a;
+
+    Node *slow = a;
+    Node *first = a;
+    bool flag = false;
+
+    while (true)
+    {
+        slow = slow->next;
+        first = first->next->next;
+        if (slow == first)
+        {
+            // cycle detected
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag)
+        cout << "Cycle deceted";
+    else
+        cout << "Cycle Not deceted";
+
+    return 0;
+}
